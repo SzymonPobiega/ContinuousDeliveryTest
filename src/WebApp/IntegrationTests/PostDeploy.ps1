61b4030fb2587140ad9b93e76b4f7927d8bb6715
SzymonPobiega <szymon.pobiega@gmail.com>
2014-02-19 11:03:43 +0100

    Add automated loading of test results

--- src/WebApp/IntegrationTests/PostDeploy.ps1
+++ src/WebApp/IntegrationTests/PostDeploy.ps1
@@ -1,2 +1,3 @@
 ﻿.\nunit-console.exe .\IntegrationTests.dll
 Copy-Item TestResult.xml C:\Projects\ContinuousDeliveryTest-TestResults
+wget http://localhost:8099/guestAuth/action.html?add2Queue=ContinuousDeliveryTest_ImportIntegrationTestResults
