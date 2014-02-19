b300a726e1c6921af1d90e1ef80f546d06c76821
SzymonPobiega <szymon.pobiega@gmail.com>
2014-02-19 11:27:29 +0100

    Switched from wget to http client

--- src/WebApp/IntegrationTests/PostDeploy.ps1
+++ src/WebApp/IntegrationTests/PostDeploy.ps1
@@ -1,3 +1,4 @@
 ﻿.\nunit-console.exe .\IntegrationTests.dll
 Copy-Item TestResult.xml C:\Projects\ContinuousDeliveryTest-TestResults
-wget -q -O- http://localhost:8099/guestAuth/action.html?add2Queue=ContinuousDeliveryTest_ImportIntegrationTestResults 2>&1 | Out-Null
+$clnt = new-object System.Net.WebClient
+$clnt.DownloadData("http://localhost:8099/guestAuth/action.html?add2Queue=ContinuousDeliveryTest_ImportIntegrationTestResults")
