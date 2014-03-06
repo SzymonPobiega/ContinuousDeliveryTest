c3d3061fa20b9ad29a715db9fd03c5ff2bae3470
SzymonPobiega <szymon.pobiega@gmail.com>
2014-03-06 14:17:52 +0100

    Updated postdeploy.ps1

--- src/WebApp/IntegrationTests/PostDeploy.ps1
+++ src/WebApp/IntegrationTests/PostDeploy.ps1
@@ -1,4 +1,4 @@
 ﻿.\nunit-console.exe .\IntegrationTests.dll
-Copy-Item TestResult.xml C:\Projects\ContinuousDeliveryTest-TestResults
+Copy-Item TestResult.xml "\\INFN1069-CI\TestResults\TestResult-$($OctopusParameters['Octopus.Project.Name']).xml"
 $clnt = new-object System.Net.WebClient
-$clnt.DownloadData("http://localhost:8099/guestAuth/action.html?add2Queue=ContinuousDeliveryTest_ImportIntegrationTestResults")
+$clnt.DownloadData("$($OctopusParameters['TeamCityURL'])/guestAuth/action.html?add2Queue=$($OctopusParameters['ImportTestResultsBuild'])")
