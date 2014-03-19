1e2ed5f3b4241f068a43243974a2284dce86d35d
SzymonPobiega <szymon.pobiega@gmail.com>
2014-03-19 10:47:25 +0100

    Added config files, replaced octopack with new version

--- src/WebApp/IntegrationTests/PostDeploy.ps1
+++ src/WebApp/IntegrationTests/PostDeploy.ps1
@@ -1,5 +1,4 @@
 ﻿.\nunit-console.exe .\IntegrationTests.dll
-[Security.Principal.WindowsIdentity]::GetCurrent()
 Copy-Item TestResult.xml "\\INFN1069-CI\TestResults\TestResult-$($OctopusParameters['Octopus.Project.Name']).xml"
 $clnt = new-object System.Net.WebClient
 $clnt.DownloadData("$($OctopusParameters['TeamCityURL'])/guestAuth/action.html?add2Queue=$($OctopusParameters['ImportTestResultsBuild'])")
