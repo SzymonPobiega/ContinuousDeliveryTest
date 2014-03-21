7ac13b63a4c7916d6d041ad565194634cc3698b9
SzymonPobiega <szymon.pobiega@gmail.com>
2014-03-21 09:45:51 +0100

    Added diagnostic

--- src/WebProjectTemplate/IntegrationTests/PostDeploy.ps1
+++ src/WebProjectTemplate/IntegrationTests/PostDeploy.ps1
@@ -1,4 +1,5 @@
 ﻿.\nunit-console.exe .\IntegrationTests.dll
 Copy-Item TestResult.xml "\\INFN1069-TeamC\TestResults\TestResult-$($OctopusParameters['Octopus.Project.Name']).xml"
 $clnt = new-object System.Net.WebClient
+Write-Host $($OctopusParameters['TeamCityUrl'])/guestAuth/action.html?add2Queue=$($OctopusParameters['ImportTestResultsBuild'])
 $clnt.DownloadData("$($OctopusParameters['TeamCityUrl'])/guestAuth/action.html?add2Queue=$($OctopusParameters['ImportTestResultsBuild'])")
